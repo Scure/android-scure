88fa9744a76ffb137aee2a14ad601275ed1d0b3f
sonnyochoa <0xsonny@gmail.com>
2013-11-08 15:59:29 -0800

    [FDV] Simple activity for the Friend Profile.
    This activity will hold the FriendFragment where the real work happens.

---
+++ Scure/src/com/scure/friendslist/FriendActivity.java
@@ -0,0 +1,17 @@
+package com.scure.bluetooth;
+
+import java.util.UUID;
+
+import android.support.v4.app.Fragment;
+
+public class FriendActivity extends SingleFragmentActivity {
+
+	@Override
+	protected Fragment createFragment() {
+		UUID friendId = (UUID)getIntent()
+				.getSerializableExtra(FriendFragment.EXTRA_FRIEND_ID);
+		
+		return FriendFragment.newInstance(friendId);
+	}
+
+}
